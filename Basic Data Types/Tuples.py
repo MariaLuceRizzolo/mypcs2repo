b6414ff6dba20f9017ffb6b8146f03036f5159d3
MariaLuceRizzolo <rizzolo.2018959@studenti.uniroma1.it>
2022-10-11 00:34:47 +0200

    modified

--- Basic Data Types/Tuples.py
+++ Basic Data Types/Tuples.py
@@ -1,9 +1,8 @@
-import sys
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
     tuple_=tuple(integer_list)
-    hash_= hash(tuple_)  % ((sys.maxsize + 1) * 2)
+    hash_= hash(tuple_)  
     print(hash_)
 
    
