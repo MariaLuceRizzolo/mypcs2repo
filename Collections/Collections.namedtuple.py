9fde097a11d52ca3f4e82fe27468e40876ca460f
MariaLuceRizzolo <rizzolo.2018959@studenti.uniroma1.it>
2022-10-17 18:37:58 +0200

    commit

---
+++ Collections/Collections.namedtuple.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+N, field_name= int(input()), input().split()
+
+student=namedtuple('student',field_name)
+sum_=0
+
+for i in range(N):
+    stud=input().split()
+    tmp=student(*stud)
+    sum_+=int(tmp.MARKS)
+
+print('%.2f'%(sum_/N))
