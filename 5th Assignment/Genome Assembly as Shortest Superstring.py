e04d2f79a99d7d3d24a6dd847d1358371d464355
MariaLuceRizzolo <rizzolo.2018959@studenti.uniroma1.it>
2023-01-07 09:26:30 +0100

    last commit

--- 5th Assignment/Genome Assembly as Shortest Superstring.py
+++ 5th Assignment/Genome Assembly as Shortest Superstring.py
@@ -6,14 +6,14 @@ with open ("5th Assignment/rosalind_long.txt") as file:
         seq.append(str(seq_record.seq))
 
     i = 0
-    k = int((len(seq[0]))/2+1)
+    k = int(0.5*(len(seq[0]))+1)
 
     while i == 0:
-        newseq=seq[0]
+        temp = seq[0]
         for j in range(k,2*(k-1)):
-            for seq in seq[1:]:
-                if seq[0][-j:] == seq[:j]:
-                    seq[0] = seq[0] + seq[j:]
-        if newseq == seq[0]:
+            for s in seq[1:]:
+                if s[-j:] == seq[0][:j]:
+                    seq[0] = s + seq[0][j:]
+        if temp == seq[0]:
             i = 1
     print(seq[0])
